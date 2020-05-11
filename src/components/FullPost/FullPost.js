88a8c33fbfe3febed5cda61d1eeabfb31196f366
saianis <anish@Anishs-MacBook-Pro.local>
2020-05-11 20:47:24 +0530

    added delete and update capabilities

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -13,11 +13,18 @@ class FullPost extends Component {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
                 axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.id )
                     .then( response => {
-                        // console.log(response);
+                        // console.log(response);                        
                         this.setState( { loadedPost: response.data } );
                     } );
             }
         }
+    }
+
+    deletePostHandler = () => {        
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);                
+            });
     }
 
     render () {
@@ -31,7 +38,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.content}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                    <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
